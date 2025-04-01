c69e8dfd17aaa92f4e402d5c8230f917adc9f311
Jucieu Felipe Vieira de Oliveira <166461551+Lipenaminha@users.noreply.github.com>
2025-03-31 22:36:19 -0300

    Create BusinessAccount.cs

---
+++ HERANÇA/Upcasting and Downcasting/BusinessAccount.cs
@@ -0,0 +1,30 @@
+namespace C_.Entities
+{
+    class BusinessAccount : Account
+    {
+
+        public double Loanlimit { get; set; }
+
+
+        public BusinessAccount()
+        {
+
+        }
+        public BusinessAccount(int number, string holder, double balance, double loanlimit) : base(number, holder, balance)
+        {
+            Loanlimit = loanlimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if(amount <= Loanlimit)
+            {
+                Balance += amount;
+            }
+
+        }
+
+       
+
+    }
+}
